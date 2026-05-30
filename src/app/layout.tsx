5b780fb1e32cef1cbb24670c48f46c6ae9c3fa56
Quoc Anh <quocanhto85@gmail.com>
2026-05-30 11:01:40 +0930

    Setting up Vercel Web Analytics & Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { ReduxProvider } from "@/providers/ReduxProvider";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
